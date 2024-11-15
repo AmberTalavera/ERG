fac8c792e5bcff761192d218a3d97eaf109a7d40
AmberTalavera <ambertalavera73@gmail.com>
2024-11-15 17:37:14 +0200

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using AspNetWebsite.Models;
+
+namespace AspNetWebsite.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
